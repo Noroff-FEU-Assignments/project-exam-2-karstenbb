f84a3d36dd2d283b3eb5af781a2454e18c54d3fe
Karsten Bjelde <fotball-karsten@hotmail.com>
2021-04-20 14:16:14 +0200

    got the search input to work

--- projectexam2/src/components/FetchHotels.js
+++ projectexam2/src/components/FetchHotels.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { api_url } from "../utils//Constants";
+import { Link } from "react-router-dom";
+
 const FetchHotels = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [places, setPlaces] = useState([]);
+  const [filteredHotels, setFilteredHotels] = useState([]);
+  const [filtered, setFiltered] = useState(false);
 
   useEffect(() => {
     fetch(api_url)
@@ -20,6 +24,38 @@ const FetchHotels = () => {
         }
       );
   }, []);
+  function handleSearch(e) {
+    let filterHotels = places.filter((place) => {
+      return place.title.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setFiltered(true);
+    setFilteredHotels(filterHotels);
+  }
+  const filterInput = document.querySelector(".search");
+  if (filterInput === "") {
+    setFiltered(false);
+  }
+  if (filtered === true) {
+    return (
+      <>
+        <input
+          type="text"
+          className="search"
+          name="query"
+          onChange={handleSearch}
+        ></input>
+        {filteredHotels.map((hotel) => {
+          return (
+            <>
+              <div className="place" key={hotel.id}>
+                <h1 key={hotel.id}>{hotel.title}</h1>
+              </div>
+            </>
+          );
+        })}
+      </>
+    );
+  }
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!loading) {
@@ -27,12 +63,30 @@ const FetchHotels = () => {
   } else {
     return (
       <>
-        <div>
+        <div className="form-container">
+          <form className="form">
+            <input
+              onChange={handleSearch}
+              className="search"
+              type="text"
+              placeholder="Search for places"
+            ></input>
+          </form>
+        </div>
+        <div className="container">
           {places.map((item) => (
-            <div key={item.id}>
-              <img alt={item.title} src={item.picture} />
-              {item.title} {item.price}{" "}
-              {item.recommended ? "recommended" : "not recommended"}
+            <div className="place" key={item.id}>
+              <img className="place__img" alt={item.title} src={item.picture} />
+              <h1 className="place__title">{item.title}</h1>{" "}
+              <div className="place__info">
+                <p className="place__recommendeed">
+                  {item.recommended ? "recommended" : "not recommended"}
+                </p>
+                <p className="place__price">{item.price}</p>
+              </div>
+              <Link to="/specific:id">
+                <button>See more</button>
+              </Link>
             </div>
           ))}
         </div>
--- projectexam2/src/pages/Hotels.js
+++ projectexam2/src/pages/Hotels.js
@@ -4,22 +4,11 @@ import FetchHotels from "../components/FetchHotels";
 
 const Hotels = () => {
   console.log(api_url);
-  function handleSearch(e) {
-    console.log(e.target.value);
-  }
+
   return (
     <>
       <h1>Hotels</h1>
-      <div>
-        <form>
-          <input
-            onChange={handleSearch}
-            className="search"
-            type="text"
-            placeholder="Search for places"
-          ></input>
-        </form>
-      </div>
+
       <div>
         <FetchHotels></FetchHotels>
       </div>
